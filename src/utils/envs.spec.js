9ccd93aa716dbee9c00798bcbeb302f9babe11de
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-09-14 14:02:31 +0100

    confgured eslint with mocha

--- src/utils/envs.spec.js
+++ src/utils/envs.spec.js
@@ -1,15 +1,12 @@
 import { expect } from 'chai'
 import getEnvs from './envs'
 
-// eslint-disable-next-line no-undef
 describe('envs', () => {
-    // eslint-disable-next-line no-undef
     it('should contain "port"', () => {
         const { envs } = getEnvs
         expect(envs).to.have.property('PORT')
     })
     
-    // eslint-disable-next-line no-undef
     it('should contain "databaseURL"', () => {
         const { envs } = getEnvs
         expect(envs).to.have.property('databaseURL')
--- src/utils/transactions/addCard.spec.js
+++ src/utils/transactions/addCard.spec.js
@@ -1,13 +1,10 @@
 import { expect } from 'chai'
 
-// eslint-disable-next-line no-undef
 describe('envs', () => {
-    // eslint-disable-next-line no-undef
     it('should test for equalness', () => {
         expect(1).to.be.eq(1)
     })
 
-    // eslint-disable-next-line no-undef
     it('should test for unequalness', () => {
         expect(1).to.not.be.eq(2)
     })
